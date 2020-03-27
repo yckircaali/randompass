a582ed8e7d1d04ed9bb174cedee9b7198c2c8644
Yucel Can Kircaali <34143353+yckircaali@users.noreply.github.com>
2020-03-27 18:55:44 +0300

    Update rndpass.py

--- rndpass.py
+++ rndpass.py
@@ -1,46 +1,46 @@
 import sys
 from random import *
 
-karakter = "abcdefghijklmnoprstuvyzwxABCDEFGHIJKLMNOPQRSTUVWXYZ"
-sayi = "0123456789"
-ozel = "'!#@.,$"
+character = "abcdefghijklmnoprstuvyzwxABCDEFGHIJKLMNOPQRSTUVWXYZ"
+number = "0123456789"
+custom = "'! # @., $"
 
-answer = input("Parolanın içerisinde sayı olsun mu? [E/H]")
+answer = input ("Is there a number in the password? [Y / N]")
 if answer == "E":
-    print("Sayi eklendi.")
+    print ("Number added.")
 elif answer == "H":
-    print("Sayi eklenmedi.")
+    print ("No numbers added.")
 
-answer2 = input("Parolanın içerisinde harf olsun mu? [E/H]")
+answer2 = input ("Is there a letter in the password? [Y / N]")
 if answer2 == "E":
-    print("Harf eklendi.")
+    print ("Letter added.")
 elif answer2 == "H":
-    print("Harf eklenmedi.")
+    print ("No letters added.")
 
-answer3 = input("Parolanın içerisinde ozel karakter olsun mu? [E/H]")
+answer3 = input ("Is there any special character in the password? [E / H]")
 if answer3 == "E":
-    print("Ozel karakter eklendi.")
+    print ("Special character added.")
 elif answer3 == "H":
-    print("Ozel karakter eklenmedi.")
+    print ("No special characters added.")
 
 if answer == "E" and answer2 == "E" and answer3 == "E":
-    hepsi = sayi + karakter + ozel
+    all = number + character + special
 if answer == "E" and answer2 == "E" and answer3 == "H":
-    hepsi = sayi + karakter
+    all = number + character
 if answer == "E" and answer2 == "H" and answer3 == "E":
-    hepsi = sayi + ozel
+    all = number + special
 if answer == "E" and answer2 == "H" and answer3 == "H":
-    hepsi = sayi
+    all = number
 if answer == "H" and answer2 == "E" and answer3 == "E":
-    hepsi = karakter + ozel
+    all = character + special
 if answer == "H" and answer2 == "E" and answer3 == "H":
-    hepsi = karakter
+    all = character
 if answer == "H" and answer2 == "H" and answer3 == "E":
-    hepsi = ozel
+    all = special
 if answer == "H" and answer2 == "H" and answer3 == "H":
-    print("Lutfen bir girdi seciniz.")
-    sys.exit()
-min = int(input("Min:"))
-max = int(input("Max:"))
-sifre ="".join(choice(hepsi) for x in range(randint(min, max)))
-print("Sifreniz =",sifre)
+    print ("Please select an entry.")
+    sys.exit ()
+min = int (input ("Min:"))
+max = int (input ("Max:"))
+password = "". join (choice (all) for x in range (randint (min, max)))
+print ("Your password =", password)
