abe2ed7e3384926b30c5a00015dc90d5a16a5b0e
Yucel Can Kircaali <tomvetom35@gmail.com>
2020-03-28 14:48:36 +0300

    Update rndpass.py

--- rndpass.py
+++ rndpass.py
@@ -7,39 +7,39 @@ custom = "'! # @., $"
 
 answer = input ("Is there a number in the password? [Y / N]")
 if answer == "E":
-    print ("Number added.")
+    print("Number added.")
 elif answer == "H":
-    print ("No numbers added.")
+    print("No numbers added.")
 
 answer2 = input ("Is there a letter in the password? [Y / N]")
 if answer2 == "E":
-    print ("Letter added.")
+    print("Letter added.")
 elif answer2 == "H":
-    print ("No letters added.")
+    print("No letters added.")
 
 answer3 = input ("Is there any special character in the password? [E / H]")
 if answer3 == "E":
-    print ("Special character added.")
+    print("Special character added.")
 elif answer3 == "H":
-    print ("No special characters added.")
+    print("No special characters added.")
 
-if answer == "E" and answer2 == "E" and answer3 == "E":
-    all = number + character + special
-if answer == "E" and answer2 == "E" and answer3 == "H":
-    all = number + character
-if answer == "E" and answer2 == "H" and answer3 == "E":
-    all = number + special
-if answer == "E" and answer2 == "H" and answer3 == "H":
-    all = number
-if answer == "H" and answer2 == "E" and answer3 == "E":
-    all = character + special
-if answer == "H" and answer2 == "E" and answer3 == "H":
-    all = character
-if answer == "H" and answer2 == "H" and answer3 == "E":
-    all = special
-if answer == "H" and answer2 == "H" and answer3 == "H":
-    print ("Please select an entry.")
-    sys.exit ()
+if answer == "Y" and answer2 == "Y" and answer3 == "Y":
+    all = number + character + special
+if answer == "Y" and answer2 == "Y" and answer3 == "N":
+    all = number + character
+if answer == "Y" and answer2 == "N" and answer3 == "Y":
+    all = number + special
+if answer == "Y" and answer2 == "N" and answer3 == "N":
+    all = number
+if answer == "N" and answer2 == "Y" and answer3 == "Y":
+    all = character + special
+if answer == "N" and answer2 == "Y" and answer3 == "N":
+    all = character
+if answer == "N" and answer2 == "N" and answer3 == "Y":
+    all = special
+if answer == "N" and answer2 == "N" and answer3 == "N":
+    print ("Please select an entry.")
+    sys.exit ()
 min = int (input ("Min:"))
 max = int (input ("Max:"))
 password = "". join (choice (all) for x in range (randint (min, max)))
