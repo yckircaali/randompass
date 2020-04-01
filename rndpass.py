0559f49299b95c7e08cbe179921232e2f842076c
Yucel Can Kircaali <tomvetom35@gmail.com>
2020-04-01 22:53:05 +0300

    Update rndpass.py

--- rndpass.py
+++ rndpass.py
@@ -3,41 +3,41 @@ from random import *
 
 character = "abcdefghijklmnoprstuvyzwxABCDEFGHIJKLMNOPQRSTUVWXYZ"
 number = "0123456789"
-custom = "'! # @., $ - _"
+custom = "' !#@.,$-_:;"
 
 answer = input ("Is there a number in the password? [Y / N]")
-if answer == "Y":
+if answer in ('y','Y'):
     print("Number added.")
-elif answer == "N":
+elif answer in ('n','N'):
     print("No numbers added.")
 
 answer2 = input ("Is there a letter in the password? [Y / N]")
-if answer2 == "Y":
+if answer2 in ('y','Y'):
     print("Letter added.")
-elif answer2 == "N":
+elif answer2 in ('n','N'):
     print("No letters added.")
 
 answer3 = input ("Is there any special character in the password? [Y / N]")
-if answer3 == "Y":
+if answer3 in ('y','Y'):
     print("Special character added.")
-elif answer3 == "N":
+elif answer3 in ('n','N'):
     print("No special characters added.")
 
-if answer == "Y" and answer2 == "Y" and answer3 == "Y":
+if answer in ('y','Y') and answer2 in ('y','Y') and answer3 in ('y','Y'):
     all = number + character + custom
-if answer == "Y" and answer2 == "Y" and answer3 == "N":
+if answer in ('y','Y') and answer2 in ('y','Y') and answer3 in ('n','N'):
     all = number + character
-if answer == "Y" and answer2 == "N" and answer3 == "Y":
+if answer in ('y','Y') and answer2 in ('n','N') and answer3 in ('y','Y'):
     all = number + custom
-if answer == "Y" and answer2 == "N" and answer3 == "N":
+if answer in ('y','Y') and answer2 in ('n','N') and answer3 in ('n','N'):
     all = number
-if answer == "N" and answer2 == "Y" and answer3 == "Y":
+if answer in ('n','N') and answer2 in ('y','Y') and answer3 in ('y','Y'):
     all = character + custom
-if answer == "N" and answer2 == "Y" and answer3 == "N":
+if answer in ('n','N') and answer2 in ('y','Y') and answer3 in ('n','N'):
     all = character
-if answer == "N" and answer2 == "N" and answer3 == "Y":
+if answer in ('n','N') and answer2 in ('n','N') and answer3 in ('y','Y'):
     all = custom
-if answer == "N" and answer2 == "N" and answer3 == "N":
+if answer in ('n','N') and answer2 in ('n','N') and answer3 in ('n','N'):
     print ("Please select an entry.")
     sys.exit ()
 min = int (input ("Min:"))
